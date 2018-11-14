917bc6293de3172820aefd44020aad3fc32680a1
Pexton <apexton2@babson.edu>
2018-11-13 22:18:32 -0500

    Exam2018

--- exam_p1.py
+++ exam_p1.py
@@ -1,9 +1,63 @@
+import string
+def get_value(name):
+    # return the value of given name
+    values = dict()
+    for index, letter in enumerate(string.ascii_letters):
+        values[letter] = index + 1
+    value = 0
+    for letter in name:
+        if letter in string.ascii_letters:
+            value = values[letter] + value
+        else:
+                break
+    return value
 
+def get_name_list(file_name):
+    name_list=[]
+    #load text file, get all the first names to name_list
+    fin = open(file_name)
+    for line in fin:
+        name = line.strip()
+        name_list.append(name)
+    return name_list
+
+
+def who_has_highest_value(name_list):
+    # return the name with highest value among the given name_list
+    maxName = ''
+    maxValue = 0
+    for name in name_list:
+        if get_value(name) > maxValue:
+            maxName = name
+            maxValue = get_value(name)
+    return maxName    
+
+def get_words(file_name):
+    # return a list of words
+    word_list=[]
+    #load text file, get all the first names to name_list
+    fin = open(file_name)
+    for line in fin:
+        word = line.strip()
+        word_list.append(word)
+    return word_list
+
+
+def get_words_with_same_value(word_list, value):
+    # return a list of matched words
+    matchedWords = [];
+    for word in word_list:
+        if get_value(word) == value:
+            matchedWords.append(word)
+    if len(matchedWords) == 0:
+        return 'None'
+    else:
+        return matchedWords
 
 
 def main():
-    pass
-
+    print(who_has_highest_value(get_name_list('roster.txt')))
+    print(get_words_with_same_value(get_words('words.txt'), get_value('Alden')))
 
 if __name__ == '__main__':
-    main()
+    main();
--- exam_p4.py
+++ exam_p4.py
@@ -1,6 +1,15 @@
 """
 What is the most interesting/funny/cool thing(s) about Python that you learned from this class or from somewhere else.
-
 You can use code or a short paragraph to illustrate it.
 """
+"""
+What is Fun?
 
+During my experience with python, it has been most interesting to learn that understanding python is like understanding a foreign language.
+There are so many aspects about programming with python that serve as building blocks to learning more and more complex capabilites. This has
+challenged me and made me think more creatively about using python to solve problems and discover new and exciting applications. In specific,
+I enjoyed learning how I could apply text analysis to openly available data sources to uncover cool and interesting insights. Something I would
+love to pursue with my python knowledge is learning how to scrape social media data to understand trends that could drive process such as product/
+service pricing, identifying target market segments and inventory management. Now that I have built a foundational knowledge of python, I am excited
+pursue projects that I am passionate about and feel are valuable in today's online global exchange of information.
+"""
